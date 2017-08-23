35a35ca9d2130a99606c81e6675efe4c00582b4b
anthony cruz <anthonycruz34@gmail.com>
2017-08-23 17:38:02 -0400

    updated server code

--- server/main.js
+++ server/main.js
@@ -1,7 +1,12 @@
 var express = require('express'),
-engines = require('consolidate');
+engines = require('consolidate'),
+http = require('http');
+
+var PORT = 7778;
+
 
 var app = new express();
+var server = http.createServer(app);
 app.set('views', __dirname+'./../app');
 app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
 app.set('view engine', 'html'); //register .html extension as template engine so we can render .html pages
@@ -11,6 +16,9 @@ app.use(express.static(__dirname + './../.tmp'));
 
 
 app.get("/", function(req, res){
+      console.log('~ Request received:', req.method , req.url);
       res.render('index.html', {});
-})
-.listen(7778);
+});
+server.listen(PORT, () =>{
+      console.log('~ Server listening on port' + PORT);
+});
