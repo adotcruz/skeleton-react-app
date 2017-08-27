a3bc873fed2b9819dfaf37566320442f4505f333
anthony cruz <anthonycruz34@gmail.com>
2017-08-27 02:53:06 -0400

    more updates

--- app/components/userCard.jsx
+++ app/components/userCard.jsx
@@ -8,12 +8,25 @@ class UserCard extends React.Component{
                               <div className="main-card">
                                     <div className="row">
                                           <div className="col-xs-4">
-                                                <div className="center-div">
+                                                <div className="center-div img-div">
                                                       <img className="img-circle" src="./assets/cat-picture.jpeg"/>
                                                 </div>
                                           </div>
                                           <div className="col-xs-7 col-xs-offset-1">
                                                 <h2> Anthony Cruz</h2>
+                                                <div className='description'>
+                                                      <p>
+                                                            I am currently a Senior at Brown University concentrating
+                                                            in Computer Science. I plan to graduate with an A.B in
+                                                            2018. I am a first generation college student, who learned
+                                                            how to code during my sophomore year of college. it has
+                                                            become one of my passions, and an activity that has taken
+                                                            over my life. I spent the last summer working at Goldman
+                                                            Sachs Technology as a Front End Engineer. I have become
+                                                            increasingly interested in Robotics, so I also have some
+                                                            research to add to to my projects. Have fun!
+                                                      </p>
+                                                </div>
                                           </div>
                                     </div>
                               </div>
